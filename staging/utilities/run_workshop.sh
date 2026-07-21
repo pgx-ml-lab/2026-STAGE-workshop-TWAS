36cb57a936101a686433f89a0bc13aa3d3ab83de
Marc-André Legault <legaultmarc@gmail.com>
2026-07-21 11:46:01 -0400

    Fixed static package ref in the workshop initialization script.

--- staging/utilities/run_workshop.sh
+++ staging/utilities/run_workshop.sh
@@ -2,13 +2,14 @@
 
 function get_data() {
     # Get the assets from the latest release and untar them.
-    release_data=$(curl -s https://api.github.com/repos/pgx-ml-lab/2025-STAGE-workshop-TWAS/releases)
+    release_data=$(curl -s https://api.github.com/repos/pgx-ml-lab/2026-STAGE-workshop-TWAS/releases)
     
     downloaded_files=$(echo "$release_data" | jq -r '.[0].assets[].browser_download_url' | xargs -n 1 basename)
     
     echo "$release_data" | jq -r '.[0].assets[].browser_download_url' | xargs -n 1 curl -LO
 
     for file in *.tar* *.tgz; do
+        echo "Processing released file $file"
         [ -f "$file" ] && tar -xaf "$file" --keep-old-files
     done
     
